ffcd333a0fecbdcaa8a43b6e7a0c6db8ae051b7b
Alexander Jährling <jaehrling@gmx.eu>
2022-05-31 22:20:58 +0200

    Initial Commit

---
+++ Shared/searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchApp.swift
+//  Shared
+//
+//  Created by Alexander Jährling on 31.05.22.
+//
+
+import SwiftUI
+
+@main
+struct searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
